3b590497bff27156083ab55eaaa4bfae28687fac
Márcio Xerém <mxerem@icloud.com>
2025-10-18 16:25:01 -0300

    terceiro commit

---
+++ ex4.py
@@ -0,0 +1,20 @@
+letra = input("Digite uma letra:")
+
+if letra == "a":
+    print(f'A letra "{letra}" é uma vogal')
+
+elif letra == "e":
+    print(f'A letra "{letra}" é uma vogal')
+
+elif letra == "i":
+    print(f'A letra "{letra}" é uma vogal')
+
+elif letra == "o":
+    print(f'A letra "{letra}" é uma vogal')
+
+elif letra == "u":
+    print(f'A letra "{letra}" é uma vogal')
+
+else:
+    print(f'A letra "{letra}" NÃO é uma vogal')
+
---
+++ ex5.py
@@ -0,0 +1,7 @@
+letra = input("Digite uma letra:")
+
+if letra =="a" or letra =="e" or letra =="i" or letra =="o" or letra =="u":
+    print(f"A letra '{letra} é vogal")
+elif letra =="b" or letra =="c" or letra =="d" or letra =="f" or letra =="g" letra =="h" or letra =="j" or letra =="k" or letra =="l" or letra =="m" letra =="n" or letra =="p" or letra =="p" or letra =="r" or letra =="s":
+else:
+    print(f"A letra '{letra}' é vogal")
