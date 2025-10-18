7a1eb30b84643b3da2fff4a35701dc3f675a2154
Márcio Xerém <mxerem@icloud.com>
2025-10-18 15:24:28 -0300

    primeiro commit

---
+++ ex1.py
@@ -0,0 +1,15 @@
+num1 = int(input('Digite o primeiro número:'))
+num2 = int(input('Digite o segundo número:'))
+num3 = int(input('Digite o terceiro número:'))
+
+maior = num1
+
+if num2 > maior:
+
+    maior = int(num2)
+
+if num3 > maior:
+    maior = num3
+
+print('O maior número é: ' + str(maior))
+# print('O maior número é: ' + maior)
